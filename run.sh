e6acf028f8197cbf83a84552982dc96257335aac
CipherX-SENATOR <111040304+CipherX-SENATOR@users.noreply.github.com>
2022-09-08 07:33:10 -0700

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,10 @@
+apt install python3.9
+apt update
+python -m pip install --upgrade pip
+pip install aiohttp
+pip install requests
+pip install pycryptodome
+pip install selfCipherX
+apt update
+clear
+python bot.py
